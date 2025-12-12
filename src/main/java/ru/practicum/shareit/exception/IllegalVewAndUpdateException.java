348a76a2c633123fe16c9be9b4f8bcb5fd67e26e
munuprig <egorprygun@ya.ru>
2025-12-12 19:37:58 +0700

    Версия 4.0.1

--- src/main/java/ru/practicum/shareit/exception/IllegalVewAndUpdateException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class IllegalVewAndUpdateException extends RuntimeException {
-    public IllegalVewAndUpdateException(String message) {
-        super(message);
-    }
-}
