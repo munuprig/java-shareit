9334f18ff95bbd47a12f5f7d0069ff1959ad1352
munuprig <egorprygun@ya.ru>
2025-12-15 12:21:31 +0700

    Версия 4.3.5

--- server/src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ server/src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.user.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.exception.EntityNotFoundException;
@@ -35,7 +34,7 @@ public class UserService {
         return UserMapper.toUserDto(user);
     }
 
-    public UserDto saveNewUser(@NotNull UserDto userDto) {
+    public UserDto saveNewUser(UserDto userDto) {
         log.info("Создание нового пользователя {}", userDto.getName());
         User user = userRepository.save(UserMapper.toUser(userDto));
         return UserMapper.toUserDto(user);
