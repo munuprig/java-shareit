ce5164c9372f87b64796a329adb7150440f904c5
munuprig <egorprygun@ya.ru>
2025-12-15 12:23:39 +0700

    Версия 4.3.5

--- server/src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ server/src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.user.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.service.UserService;
 import ru.practicum.shareit.user.dto.UserDto;
